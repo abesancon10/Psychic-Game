eb33a812c0b1520a9c0af9b908be6745d21ac9f8
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-03-18 12:42:00 -0500

    added the function onkeyup with the variable userGuess = event.key; console.log the function; console is recording keys pressed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,37 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerChoices = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 var win = 0;
 var losses = 0;
 var guessLeft = 9;
-var guessChoices = []
+var guessChoices = [];
+
+document.onkeyup = function(event) {
+  var userGuess = event.key;
+  console.log(userGuess)
+};
