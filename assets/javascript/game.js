109b22db28b5bcccf27ed1a6d1a914f7a82b9aa6
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-03-18 11:44:04 -0500

    added the values to the variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var win = []
-var losses = []
-var guessLeft = []
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var win = 0;
+var losses = 0;
+var guessLeft = 9;
 var guessChoices = []
