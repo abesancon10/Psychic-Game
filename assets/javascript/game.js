052cfda100ba6e9ecdf3068c26a168a796c3a480
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-03-18 16:50:54 -0500

    added if conditional statement for if user guesses computers letter correctly and -1 guess if incorrectly guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -34,8 +34,46 @@ var guessChoices = [];
 document.onkeyup = function(event) {
   var userGuess = event.key;
 
-  var computerAnswer = computerChoices [Math.floor(Math.random() * computerChoices.length)];
-  console.log(computerAnswer)
-  console.log(userGuess)
-  
-};
+  var computerAnswer =
+    computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  var userChoices = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+
+  if (userChoices.indexOf(userGuess) > -1) {
+    if (userGuess === computerAnswer) {
+      win++;
+      guessLeft = 9;
+      guessChoices = [];
+    }
+}
+    if (userGuess !== computerAnswer) {
+        guessLeft = -1;
+    }
+  }
+
