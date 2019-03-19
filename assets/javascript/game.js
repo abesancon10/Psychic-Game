7045ff8aae234a0817fe4c72437b10796e3b7b1b
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-03-19 18:53:13 -0500

    cleaning up code, deleted userchoises; var was not being used and was taking up to many lines

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,61 +26,60 @@ var computerChoices = [
   "y",
   "z"
 ];
-var userChoices = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z"
-  ];
 var win = 0;
 var losses = 0;
 var guessLeft = 9;
-var guessChoices = [];
+var guessChoices = ["a",
+"b",
+"c",
+"d",
+"e",
+"f",
+"g",
+"h",
+"i",
+"j",
+"k",
+"l",
+"m",
+"n",
+"o",
+"p",
+"q",
+"r",
+"s",
+"t",
+"u",
+"v",
+"w",
+"x",
+"y",
+"z"];
 
 document.onkeyup = function(event) {
-  var userChoices = event.key;
+  var userGuess = event.key;
 
   var computerAnswer =
     computerChoices[Math.floor(Math.random() * computerChoices.length)];
   //causes answer to change every time a button is clicked
 
+  var userChoices = [
+    
+  ];
 
   if (userGuess === computerAnswer) {
     win++;
     guessLeft = 9;
-    guessChoices = [];
-  } else if (userGuess !== computerAnswer) {
+  } if (userGuess !== computerAnswer) {
     guessLeft--
 
   }
-  //it works now; experimenting with loops = waste for this
-  else if (guessLeft === 0) {
+  
+  if (guessLeft === 0) {
     losses++;
     guessLeft = 9;
     guessChoices = [];
-  } //try a loop
+  } 
   console.log(guessLeft);
   console.log(userGuess);
   console.log(win);
